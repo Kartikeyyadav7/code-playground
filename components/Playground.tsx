81b7df9d3373c25a8d3fc3c41c27dc1ab0d2062e
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2022-02-01 22:09:27 +0530

    added xterm

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -1,15 +1,38 @@
+// import { useRef } from "react";
 import Split from "react-split";
+import Editor from "@monaco-editor/react";
+import dynamic from "next/dynamic";
 
 const Playground = () => {
+    const Xterm = dynamic(() => import("./Xterm"), {
+        ssr: false,
+    });
     return (
         <>
             <Split className="flex" style={{ height: `100vh` }}>
-                <div className="bg-gray-200">Files</div>
+                <div className="bg-black flex flex-col">
+                    <button className="bg-white">index.html</button>
+                    <button className="bg-white">style.css</button>
+                    <button className="bg-white">script.js</button>
+                </div>
                 <Split direction="vertical" style={{ height: `100vh` }}>
-                    <div className="bg-gray-500">Editor</div>
-                    <div className="bg-gray-600">Terminal</div>
+                    <Editor
+                        // height="80vh"
+                        theme="vs-dark"
+                        // path={pathName}
+                        // defaultLanguage={langName}
+                        // defaultValue={valueName}
+                        // onChange={handleEditorChange}
+                        defaultLanguage="javascript"
+                        defaultValue="// ok lets start"
+                    />
+                    <Xterm />
                 </Split>
-                <div className="bg-gray-700">Iframe</div>
+                <iframe
+                    src="http://localhost:8080"
+                    onLoad={() => console.log("loaded")}
+                    onError={() => console.log("error")}
+                ></iframe>
             </Split>
         </>
     );
